9fb939d84ddb6d434b28cb28f220ece2e5f2b1a2
giadarol <giovanni.iadarola@cern.ch>
2017-04-28 08:12:51 +0200

    Moved inverse boost to module

--- 002_test_boost.py
+++ 002_test_boost.py
@@ -34,7 +34,7 @@ print "Check sigma", sigma_st, track[4], np.abs(sigma_st - track[4])
 print "Check delta", delta_st, track[5], np.abs(delta_st - track[5]) 
 
 
-# test inverse boost (sixtrack vs initial)
+# test inverse boost (sixtrack)
 bs.boosti(sphi,cphi,tphi,salpha,calpha,track)
 
 print '\n\n ***** Inverse boost (initial vs sixtrack)'
@@ -46,38 +46,10 @@ print "Check sigma", sigma, track[4], np.abs(sigma - track[4])
 print "Check delta", delta, track[5], np.abs(delta - track[5]) 
 
 
-pz_st = np.sqrt((1.+delta_st)**2-px_st**2-py_st**2)
-hx_st = px_st/pz_st
-hy_st = py_st/pz_st
-hsigma_st = 1.-(delta_st+1)/pz_st
-
-Det_L = 1./cphi + (hx_st*calpha + hy_st*salpha-hsigma_st*sphi)*tphi
-
-Linv_11 = (1./cphi + salpha*tphi*(hy_st-hsigma_st*salpha*sphi))/Det_L
-Linv_12 = (salpha*tphi*(hsigma_st*calpha*sphi-hx_st))/Det_L
-Linv_13 = -tphi*(calpha - hx_st*salpha*salpha*sphi + hy_st*calpha*salpha*sphi)/Det_L
-
-Linv_21 = (calpha*tphi*(-hy_st + hsigma_st*salpha*sphi))/Det_L
-Linv_22 = (1./cphi + calpha*tphi*(hx_st-hsigma_st*calpha*sphi))/Det_L
-Linv_23 = -tphi*(salpha - hy_st*calpha*calpha*sphi + hx_st*calpha*salpha*sphi)/Det_L
-
-Linv_31 = -hsigma_st*calpha*sphi/Det_L
-Linv_32 = -hsigma_st*salpha*sphi/Det_L
-Linv_33 = (1. + hx_st*calpha*sphi + hy_st*salpha*sphi)/Det_L
-
-x_i = Linv_11*x_st + Linv_12*y_st + Linv_13*sigma_st
-y_i = Linv_21*x_st + Linv_22*y_st + Linv_23*sigma_st
-sigma_i = Linv_31*x_st + Linv_32*y_st + Linv_33*sigma_st
-
-h = (delta_st+1.-pz_st)*cphi*cphi
-
-px_i = px_st*cphi+h*calpha*tphi
-py_i = py_st*cphi+h*salpha*tphi
-
-delta_i = delta_st + px_i*calpha*tphi + py*salpha*tphi - h*tphi*tphi
+# test inverse boost (python)
+x_i, px_i, y_i, py_i, sigma_i, delta_i = bo.inv_boost(x_st, px_st, y_st, py_st, sigma_st, delta_st, parboost)
 
 
-# test inverse boost (python vs initial)
 print '\n\n ***** Inverse boost (initial vs python)'
 print "Check x", x, x_i, np.abs(x-x_i) 
 print "Check px", px, px_i, np.abs(px-px_i) 
--- boost.py
+++ boost.py
@@ -48,5 +48,48 @@ def boost(x, px, y, py, sigma, delta, parboost):
     
     return x_st, px_st, y_st, py_st, sigma_st, delta_st
     
+def inv_boost(x_st, px_st, y_st, py_st, sigma_st, delta_st, parboost):
+    
+    sphi = parboost.sphi
+    cphi = parboost.cphi
+    tphi = parboost.tphi
+    salpha = parboost.salpha
+    calpha = parboost.calpha
+    
+    
+    pz_st = np.sqrt((1.+delta_st)**2-px_st**2-py_st**2)
+    hx_st = px_st/pz_st
+    hy_st = py_st/pz_st
+    hsigma_st = 1.-(delta_st+1)/pz_st
+
+    Det_L = 1./cphi + (hx_st*calpha + hy_st*salpha-hsigma_st*sphi)*tphi
+
+    Linv_11 = (1./cphi + salpha*tphi*(hy_st-hsigma_st*salpha*sphi))/Det_L
+    Linv_12 = (salpha*tphi*(hsigma_st*calpha*sphi-hx_st))/Det_L
+    Linv_13 = -tphi*(calpha - hx_st*salpha*salpha*sphi + hy_st*calpha*salpha*sphi)/Det_L
+
+    Linv_21 = (calpha*tphi*(-hy_st + hsigma_st*salpha*sphi))/Det_L
+    Linv_22 = (1./cphi + calpha*tphi*(hx_st-hsigma_st*calpha*sphi))/Det_L
+    Linv_23 = -tphi*(salpha - hy_st*calpha*calpha*sphi + hx_st*calpha*salpha*sphi)/Det_L
+
+    Linv_31 = -hsigma_st*calpha*sphi/Det_L
+    Linv_32 = -hsigma_st*salpha*sphi/Det_L
+    Linv_33 = (1. + hx_st*calpha*sphi + hy_st*salpha*sphi)/Det_L
+
+    x_i = Linv_11*x_st + Linv_12*y_st + Linv_13*sigma_st
+    y_i = Linv_21*x_st + Linv_22*y_st + Linv_23*sigma_st
+    sigma_i = Linv_31*x_st + Linv_32*y_st + Linv_33*sigma_st
+
+    h = (delta_st+1.-pz_st)*cphi*cphi
+
+    px_i = px_st*cphi+h*calpha*tphi
+    py_i = py_st*cphi+h*salpha*tphi
+
+    delta_i = delta_st + px_i*calpha*tphi + py_i*salpha*tphi - h*tphi*tphi
+
+
+    
+    return x_i, px_i, y_i, py_i, sigma_i, delta_i    
+    
         
     
