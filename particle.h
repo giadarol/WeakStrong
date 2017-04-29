2395d2e85f49576e517a2ae78e0ba239447f4925
giadarol <giovanni.iadarola@cern.ch>
2017-04-29 19:07:42 +0200

    Removed all cython stuff and...
     what is now in sixtracklib

--- particle.h
+++
@@ -1,6 +0,0 @@
-typedef struct{
-	double x;
-    double px;
-    double y;
-    double py;
-} particle;
--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize([Extension("python_wrapper", ["python_wrapper.pyx", 'weak_strong_4d_c.c', 'faddeeva_cern.c', 
-                                                            'transverse_field_gauss_round.c', 'cmpx.c', 'transverse_field_gauss_ellip.c'], 
-								libraries=[])])
-)
