ef9f4df56118f1d7dd391f34cf6c768677e0bb04
giadarol <giovanni.iadarola@cern.ch>
2017-08-08 17:44:31 +0200

    Still cleaning up tests  Changes to be committed: 	modified:   006_test_slicing_against_sixtrack.py

--- 006_test_slicing_against_sixtrack.py
+++ 006_test_slicing_against_sixtrack.py
@@ -5,6 +5,8 @@ import slicing_sixtrack
 import slicing as slc
 import boost as bo
 
+import mystyle as ms
+
 
 #~ sigmaz = .075
 #~ N_slices = 7
@@ -48,20 +50,58 @@ x_st, px_st, y_st, py_st, sigma_st, delta_st = bo.boost(x=0*z_centroids, px=0*z_
 
 
 pl.close('all')
-pl.figure(1)
-pl.subplot(3,1,1)
-pl.plot(sigma_st, '-ob')
-pl.plot(z_centr_strk, 'vr')
-pl.subplot(3,1,2)
-pl.plot(x_st, '-ob')
+fontsz = 14
+lw = 2
+ms.mystyle_arial(fontsz=fontsz, dist_tick_lab=5)
+
+
+fig1 = pl.figure(1)
+fig1.set_facecolor('w')
+
+
+ax1 = pl.subplot(3,1,1)
+pl.plot(sigma_st, '-ob', lw=lw, label='Library')
+pl.plot(z_centr_strk, 'vr', label='Sixtrack')
+ax2 = pl.subplot(3,1,2, sharex=ax1)
+pl.plot(x_st, '-ob', lw=lw)
 pl.plot(x_centr_strk, 'vr')
-pl.subplot(3,1,3)
-pl.plot(y_st, '-ob')
+ax3 = pl.subplot(3,1,3, sharex=ax1)
+pl.plot(y_st, '-ob', lw=lw)
 pl.plot(y_centr_strk, 'vr')
 
-pl.figure(100)
-pl.subplot(3,1,1)
-pl.plot((sigma_st - z_centr_strk)/sigma_st, '.-')
+for ax in [ax1,ax2,ax3]:
+    ax.grid('on')
+    
+ax3.set_xlabel('# slice')
+ax1.set_ylabel('z* [m]')
+ax2.set_ylabel('x* [m]')
+ax3.set_ylabel('y* [m]')
+
+ax1.legend(loc='best', prop={'size':fontsz})
+
+
+fig100 = pl.figure(100)
+fig100.set_facecolor('w')
+ax21 = pl.subplot(3,1,1, sharex=ax1)
+pl.plot((sigma_st - z_centr_strk)/sigma_st, '.-', lw=lw)
+ms.sciy()
+ax22 = pl.subplot(3,1,2, sharex=ax1)
+pl.plot((x_st - x_centr_strk)/x_st, '.-', lw=lw)
+ms.sciy()
+ax23 = pl.subplot(3,1,3, sharex=ax1)
+pl.plot((y_st - y_centr_strk)/x_st, '.-', lw=lw)
+ms.sciy()
+
+for ax in [ax21,ax22,ax23]:
+    ax.grid('on')
+
+    
+ax23.set_xlabel('# slice')
+ax21.set_ylabel('Error on z* [m]')
+ax22.set_ylabel('Error on x* [m]')
+ax23.set_ylabel('Error on y* [m]')
+
+fig100.subplots_adjust(top=.95, hspace=.35)
 
 
 pl.show()
