698328e9f22163ac72cfc55d7b04ba85f1308228
giadarol <giovanni.iadarola@cern.ch>
2017-08-09 15:01:34 +0200

    Working on tests
    Changes to be committed:
    	modified:   mad_sigmas/002b_test_full_propagation_formula_in_drift.py

--- mad_sigmas/002b_test_full_propagation_formula_in_drift.py
+++ mad_sigmas/002b_test_full_propagation_formula_in_drift.py
@@ -18,15 +18,6 @@ bety_enter = 3000
 alfx_enter = 100
 alfy_enter = -50
 
-#~ L_line = 100
-#~ Ds = 2.
-#~ skew_at = 10.
-#~ L_skew = 1.
-#~ k_skew = -.02
-#~ betx_enter = 1500
-#~ bety_enter = 3000
-#~ alfx_enter = 100
-#~ alfy_enter = -50
 
 #~ L_line = 100
 #~ Ds = 2.
@@ -37,7 +28,7 @@ alfy_enter = -50
 #~ bety_enter = 3000
 #~ alfx_enter = -100
 #~ alfy_enter = 50
-
+#~ 
 #~ # To have a positive Sigma_13
 #~ L_line = 100
 #~ Ds = 2.
@@ -48,8 +39,8 @@ alfy_enter = -50
 #~ bety_enter = 1500
 #~ alfx_enter = -100
 #~ alfy_enter = 50
-
-# To have pass through a roung beam
+#~ 
+#~ # To have pass through a round beam
 #~ L_line = 100
 #~ Ds = 2.
 #~ skew_at = 1.
@@ -103,56 +94,158 @@ Sig_11, Sig_12, Sig_13, Sig_14, Sig_22, Sig_23, Sig_24,\
                                           
 
 
+# Propagate Sigma matrix and diagonalize
+Sigmas_at_0 = psm.Sigmas(ob.SIG11[i_ref], ob.SIG12[i_ref], ob.SIG13[i_ref], ob.SIG14[i_ref],
+                     ob.SIG22[i_ref], ob.SIG23[i_ref], ob.SIG24[i_ref], ob.SIG33[i_ref],
+                     ob.SIG34[i_ref], ob.SIG44[i_ref])
+Sig_11_hat, Sig_33_hat, costheta, sintheta, \
+    dS_Sig_11_hat, dS_Sig_33_hat, dS_costheta, dS_sintheta,\
+    extra_data = psm.propagate_Sigma_matrix_vectorized(Sigmas_at_0, S, handle_singularities=True, threshold_singular = 1e-30)
 
 
 # Plot results of the tests
 pl.close('all')
 fontsz = 14
 lw = 3
-ms.mystyle(fontsz = fontsz)
+mks = 8
+ms.mystyle_arial(fontsz = fontsz)
 
-pl.figure(1); pl.clf()
+fig1 = pl.figure(1); pl.clf()
+fig1.set_facecolor('w')
 sp0 = pl.subplot(2,1,1)
-pl.plot(S, Sig_11, 'b', label='Sig_11')
-pl.plot(ob.S-s_ref, ob.SIG11, '.c')
-pl.plot(S, Sig_33, 'r', label='Sig_33')
-pl.plot(ob.S-s_ref, ob.SIG33, '.m')
+pl.plot(S, Sig_11, 'b', label='Sig_11', lw=lw)
+pl.plot(ob.S-s_ref, ob.SIG11, '.c', markersize=mks, lw=lw)#, label='Sig_11 (MAD)'
+pl.plot(S, Sig_33, 'r', label='Sig_33', lw=lw)
+pl.plot(ob.S-s_ref, ob.SIG33, '.m', markersize=mks)#, label='Sig_33 (MAD)')
 pl.legend(loc='best', prop = {'size':fontsz})
+pl.grid('on')
 ms.sciy()
 
-pl.subplot(2,1,2, sharex=sp0)
-pl.plot(S, Sig_13, label='Sig_13')
-pl.plot(ob.S-s_ref, ob.SIG13, '.r')
+sp1 = pl.subplot(2,1,2, sharex=sp0)
+pl.plot(S, Sig_13, label='Sig_13', lw=lw)
+pl.plot(ob.S-s_ref, ob.SIG13, '.c', markersize=mks)
+pl.xlabel('s [m]')
 pl.legend(loc='best', prop = {'size':fontsz})
-ms.sciy()
-pl.suptitle('Check optics propagation against MAD-X')
-
-pl.figure(2); pl.clf()
-sp0 = pl.subplot(2,1,1)
-pl.plot(S, Sig_12, 'b', label='Sig_12')
-pl.plot(ob.S-s_ref, ob.SIG12, '.b')
-pl.plot(S, Sig_14, 'r', label='Sig_14')
-pl.plot(ob.S-s_ref, ob.SIG14, '.m')
-pl.plot(S, Sig_23, 'g', label='Sig_23')
-pl.plot(ob.S-s_ref, ob.SIG23, '.g')
-pl.plot(S, Sig_34, 'c', label='Sig_34')
-pl.plot(ob.S-s_ref, ob.SIG34, '.c')
-
-pl.legend(loc='best', prop = {'size':fontsz})
-ms.sciy()
-
-pl.subplot(2,1,2, sharex=sp0)
-pl.plot(S, Sig_22, label='Sig_22')
-pl.plot(ob.S-s_ref, ob.SIG22, '.b')
-pl.plot(S, Sig_24,'r', label='Sig_24')
-pl.plot(ob.S-s_ref, ob.SIG24, '.r')
-pl.plot(S, Sig_44,'g', label='Sig_44')
-pl.plot(ob.S-s_ref, ob.SIG44, '.g')
-pl.legend(loc='best', prop = {'size':fontsz})
+pl.grid('on')
 ms.sciy()
 pl.suptitle('Check optics propagation against MAD-X')
 
 
+
+fig101 = pl.figure(101); pl.clf()
+fig101.set_facecolor('w')
+sp0 = pl.subplot(2,1,1)
+pl.plot(S, Sig_12, 'b', label='Sig_12', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG12, '.b', lw=lw, markersize=mks)
+pl.plot(S, Sig_14, 'r', label='Sig_14', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG14, '.m', lw=lw, markersize=mks)
+pl.plot(S, Sig_23, 'g', label='Sig_23', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG23, '.g', lw=lw, markersize=mks)
+pl.plot(S, Sig_34, 'c', label='Sig_34', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG34, '.c', lw=lw, markersize=mks)
+pl.grid('on')
+
+pl.legend(loc='best', prop = {'size':fontsz})
+ms.sciy()
+
+pl.subplot(2,1,2, sharex=sp0)
+pl.plot(S, Sig_22, label='Sig_22', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG22, '.b', lw=lw, markersize=mks)
+pl.plot(S, Sig_24,'r', label='Sig_24', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG24, '.r', lw=lw, markersize=mks)
+pl.plot(S, Sig_44,'g', label='Sig_44', lw=lw, markersize=mks)
+pl.plot(ob.S-s_ref, ob.SIG44, '.g', lw=lw, markersize=mks)
+pl.xlabel('s [m]')
+pl.legend(loc='best', prop = {'size':fontsz})
+ms.sciy()
+pl.grid('on')
+pl.suptitle('Check optics propagation against MAD-X')
+
+
+#Check with linear algebra
+lam_1 = []
+lam_2 = []
+cos_1 = []
+sin_1 = []
+cos_2 = []
+sin_2 = []
+for i_s, ss in enumerate(S):
+    a = np.array([[Sig_11[i_s], Sig_13[i_s]],
+                 [Sig_13[i_s], Sig_33[i_s]]])
+    w, v = np.linalg.eig(a)
+    lam_1.append(w[0])
+    lam_2.append(w[1])
+    v1x = v[0,0]
+    v1y = v[1,0]
+    if v1x<0: 
+        v1x *= -1
+        v1y *= -1
+    v2x = v[0,1]
+    v2y = v[1,1]
+    if v2x<0: 
+        v2x *= -1
+        v2y *= -1
+    cos_1.append(v1x/np.sqrt(v1x**2+v1y**2))
+    cos_2.append(v2x/np.sqrt(v2x**2+v2y**2))
+    sin_1.append(v1y/np.sqrt(v1x**2+v1y**2))    
+    sin_2.append(v2y/np.sqrt(v2x**2+v2y**2))
+    
+fig2 = pl.figure(2); pl.clf()
+fig2.set_facecolor('w')
+pl.subplot(2,1,1, sharex=sp0)
+pl.plot(S, Sig_11_hat, 'b', label = 'Sig_11_hat', lw=lw)
+pl.plot(S, lam_1, 'c--', label= 'eigen 1', lw=lw)
+pl.plot(S, Sig_33_hat, 'r', label = 'Sig_33_hat', lw=lw)
+pl.plot(S, lam_2, 'm--', label= 'eigen 2', lw=lw)
+pl.grid('on')
+ms.sciy()
+pl.legend(loc='best', prop={'size':fontsz})
+pl.subplot(2,1,2, sharex=sp0)
+pl.plot(S, costheta, 'b', label='costheta', lw=lw)
+pl.plot(S, sintheta, 'r', label='sintheta', lw=lw)
+pl.plot(S, sin_1, 'm--', label='sin (diag)', lw=lw)
+pl.plot(S, cos_1, 'c--', label='cos (diag)', lw=lw)
+pl.xlabel('s [m]')
+pl.grid('on')
+pl.legend(loc='best', prop={'size':fontsz})
+pl.suptitle('Check rotation against matrix diagonalization\n'+\
+            'At s=0: Sig11=%.1e,Sig22=%.1e,Sig33=%.1e,Sig44=%.1e\n'%(Sigmas_at_0.Sig_11_0, Sigmas_at_0.Sig_22_0, Sigmas_at_0.Sig_33_0, Sigmas_at_0.Sig_44_0)+\
+            'Sig12=%.1e,Sig13=%.1e,Sig14=%.1e,\nSig23=%.1e,Sig24=%.1e,Sig34=%.1e'%(Sigmas_at_0.Sig_12_0, Sigmas_at_0.Sig_13_0, Sigmas_at_0.Sig_14_0, Sigmas_at_0.Sig_23_0, Sigmas_at_0.Sig_24_0, Sigmas_at_0.Sig_34_0))
+theta = np.arctan2(sintheta, costheta)
+fig2.subplots_adjust(top=.82)
+
+fig3 = pl.figure(3)
+fig3.set_facecolor('w')
+pl.plot(S, theta*180/np.pi, lw=lw)
+pl.xlabel('s [m]')
+pl.ylabel('Theta [deg]')
+pl.grid('on')
+
+
+fig4 = pl.figure(4)
+fig4.set_facecolor('w')
+pl.subplot(2,1,1, sharex=sp0)
+pl.plot(S, dS_costheta, 'm-', lw=lw)
+pl.plot(S[:-1], np.diff(costheta)/np.diff(S), 'r--', lw=lw)
+pl.plot(S, dS_sintheta, 'b-', lw=lw)
+pl.plot(S[:-1], np.diff(sintheta)/np.diff(S), 'c--', lw=lw)
+pl.ylim(-.02, .02)
+pl.grid('on')
+ms.sciy()
+
+pl.subplot(2,1,2, sharex=sp0)
+pl.plot(S, dS_Sig_11_hat, 'm-', lw=lw)
+pl.plot(S[:-1], np.diff(Sig_11_hat)/np.diff(S), 'r--', lw=lw)
+pl.plot(S, dS_Sig_33_hat, 'b-', lw=lw)
+pl.plot(S[:-1], np.diff(Sig_33_hat)/np.diff(S), 'c--', lw=lw)
+pl.suptitle('Check derivatives against finite differeces')
+pl.ylim(-1e-6, 1e-6)
+pl.grid('on')
+ms.sciy()
+
+
+for fig in [fig1, fig101, fig2, fig3, fig4]:
+    fig.subplots_adjust(bottom=.12)
 
 
 
