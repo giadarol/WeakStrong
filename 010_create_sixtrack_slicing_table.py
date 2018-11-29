e922c6e5ef0733f6e3f9c9e3753ff9c5c71757ae
giadarol <giovanni.iadarola@cern.ch>
2018-11-29 14:57:03 +0100

    Generation of table for pysixtrack

---
+++ 010_create_sixtrack_slicing_table.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pylab as pl
+
+import slicing_sixtrack
+import slicing as slc
+import boost as bo
+
+import mystyle as ms
+
+
+N_slices_max = 50
+
+sigmaz = 1.
+N_part_tot = 1.
+
+
+phi = 0.
+alpha = 0.
+cphi = 1.
+sphi = 0.
+calpha = 1.
+salpha = 0.
+
+z_centroids = np.zeros((N_slices_max+1, N_slices_max), dtype=np.float64)
+
+for N_slices in range(0, N_slices_max+1):
+
+    if N_slices == 0 :
+        continue
+
+    star = np.array(np.zeros((3,N_slices)), order='F')
+    slicing_sixtrack.stsld(star,cphi2=cphi,sphi2=sphi,sigzs=sigmaz,calpha=calpha,salpha=salpha)
+    z_centr_strk = star[2,:]
+    x_centr_strk = star[0,:]
+    y_centr_strk = star[1,:]
+
+    z_centroids[N_slices, :N_slices] = z_centr_strk
+
+np.savetxt('z_centroids_from_sixtrack.txt', z_centroids)
